6c3807f966bf75dae3a03c1fc1d06611a8d2cc79
Parasam, Kaushik N <kaushik.parasam@dematic.com>
2020-08-30 12:20:42 -0500

    Updates

---
+++ precache-manifest.4b2c7979849c5ebe9a47fdb9718380bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2a07ce8760d37917f2cbcf7e8a0362",
+    "url": "/my-first-react-app/index.html"
+  },
+  {
+    "revision": "60c91d2f1d93bbf1a533",
+    "url": "/my-first-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6cf924f1adba6cf32ce1",
+    "url": "/my-first-react-app/static/js/2.67944e0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-first-react-app/static/js/2.67944e0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60c91d2f1d93bbf1a533",
+    "url": "/my-first-react-app/static/js/main.7ca442d5.chunk.js"
+  },
+  {
+    "revision": "52f0b4f2e3a06fb00c8c",
+    "url": "/my-first-react-app/static/js/runtime-main.87669ce6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-first-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
